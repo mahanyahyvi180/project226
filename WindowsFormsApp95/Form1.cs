5aee11baa523a27cdd1aa275a043995a63228096
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-19 23:20:36 +0330

    Add files via upload

---
+++ WindowsFormsApp95/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp95
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                ((TextBox)sender).BackColor = Color.OrangeRed;
+            }
+
+            else
+            {
+                ((TextBox)sender).BackColor = Color.White;
+            }
+        }
+
+    }
+
+}
+
